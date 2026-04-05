60848f3000fa17645495bb46180679825fe7bbc5
muhammedashiqkm <ashiq.kottakuthm@gmail.com>
2026-04-05 13:24:03 +0530

    update settings

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -97,7 +97,7 @@ MEDIA_ROOT = config('MEDIA_MOUNT_PATH', default=BASE_DIR / 'media')
 # --- Email Configuration ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
